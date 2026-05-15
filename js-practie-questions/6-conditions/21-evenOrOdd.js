27e51e824fb94be518ab3f0f7211fd5c124d2ebd
dev.sagartmg <dev.sagartmg@gmail.com>
2026-05-15 07:48:48 +0545

    conditions questions

---
+++ js-practie-questions/6-conditions/21-evenOrOdd.js
@@ -0,0 +1,15 @@
+/*
+TODO: Create a function called evenOrOdd that takes a number and returns "even" if the number is even, "odd" if it's odd.
+
+Example:
+evenOrOdd(4) ➞ "even"
+evenOrOdd(7) ➞ "odd"
+evenOrOdd(0) ➞ "even"
+*/
+
+function evenOrOdd(num) {
+    // your code here
+}
+
+console.log(evenOrOdd(4));
+console.log(evenOrOdd(7));
