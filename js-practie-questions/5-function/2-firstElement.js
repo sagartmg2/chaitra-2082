0d5b0d8d11494cb382e5bbca2476cf215f2f8b23
dev.sagartmg <dev.sagartmg@gmail.com>
2026-05-15 07:49:24 +0545

    add more hints

--- js-practie-questions/5-function/2-firstElement.js
+++ js-practie-questions/5-function/2-firstElement.js
@@ -1,16 +1,10 @@
 let courses = ["mern", "python", "qa"];
+let users = ["ram", "hari", "sita"];
 
 /* TODO: Create a function that takes an array and returns the first element. 
     exmple.
 
     getFirstElement(courses) // EXPECTED-OUTPUT : mern
-
-
-
-
-
-
-
-
+    getFirstElement(users) // EXPECTED-OUTPUT : ram
 
 */
