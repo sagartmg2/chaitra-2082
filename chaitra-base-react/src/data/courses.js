4894a2308c55eeab6288505a8c1b9c720dc79878
dev.sagartmg <dev.sagartmg@gmail.com>
2026-05-31 08:10:13 +0545

    list courses in static way

---
+++ chaitra-base-react/src/data/courses.js
@@ -0,0 +1,43 @@
+export let courses = [
+  {
+    featured:true,
+    title: "MERN Stack Training in Nepal",
+    image: "/static/Images/courses/banner/mern-banner-min_nplbrgN.jpg",
+  },
+  {
+    featured:true,
+    title: "Python with Django Training in Nepal",
+    image: "/static/Images/courses/banner/python-banner-min_7hCoIdR.jpg",
+  },
+  {
+    featured:true,
+    title: "Digital Marketing Training in Nepal",
+    image:
+      "/static/Images/courses/banner/DIGITAL-MARKETING-banner-min_JBHENk4.webp",
+  },
+  {
+    featured:false,
+    title: "Quality Assurance Training in Nepal",
+    image: "/static/Images/courses/banner/qa-banner-min_l0DzbAP.jpg",
+  },
+  {
+    featured:true,
+    title: "Graphic Design Training in Nepal",
+    image: "/static/Images/courses/banner/banner11-min_6ANDad7.png",
+  },
+  {
+    featured:false,
+    title: "Flutter Training in Nepal",
+    image: "/static/Images/courses/banner/flutter-banner-min_DUDTPdH.jpg",
+  },
+  {
+    featured:false,
+    title: "DevOps Training in Nepal",
+    image: "/static/Images/courses/banner/DevOps_Training.png",
+  },
+];
+
+
+// export courses;
+
+// export default courses
--- chaitra-base-react/src/main.jsx
+++ chaitra-base-react/src/main.jsx
@@ -1,85 +1,90 @@
 import { createRoot } from "react-dom/client";
+// import {courses} from "./data/courses"
 
 let description =
   "Lorem ipsum dolor sit amet consectsequi quas fugit incidunt.";
 
 let domain = "https://mindrisers.com.np";
-let blogs = [
-  {
-    title: "Web Designing Training in Putalisadak, Kathmandu",
-    image:
-      "/static/Images/blog/single_post/Web_Designing_Training_in_Putalisadak_Kathmandu.jpg",
-  },
-  {
-    title: "Graphic Designing Training in Putalisadak, Kathmandu | Mindrisers",
-    image:
-      "/static/Images/blog/single_post/Graphic_Designing_Training_in_Putalisadak_Kathmandu__Mindrisers.png",
-  },
-  {
-    title:
-      "Data Science with Python Training in Putalisadak, Kathmandu | Mindrisers",
-    image:
-      "/static/Images/blog/single_post/Data_Science_with_Python_Training_in_Putalisadak_Kathmandu.jpg",
-  },
-  {
-    title: "UI/UX Training in Putalisadak, Kathmandu | Mindrisers",
-    image:
-      "/static/Images/blog/single_post/UIUX_Training_in_Putalisadak_Kathmandu__Mindrisers.png",
-  },
-  {
-    title: "Flutter Training in Putalisadak, Kathmandu | Mindrisers",
-    image:
-      "/static/Images/blog/single_post/Flutter_Training_in_Putalisadak_Kathmandu_Mindrisers.png",
-  },
-  {
-    title: "DevOps Training in Putalisadak, Kathmandu | Mindrisers",
-    image:
-      "/static/Images/blog/single_post/DevOps_Training_in_Putalisadak_Kathmandu__Mindrisers.png",
-  },
-];
 
 let courses = [
   {
+    featured: true,
     title: "MERN Stack Training in Nepal",
     image: "/static/Images/courses/banner/mern-banner-min_nplbrgN.jpg",
   },
   {
+    featured: true,
     title: "Python with Django Training in Nepal",
     image: "/static/Images/courses/banner/python-banner-min_7hCoIdR.jpg",
   },
   {
+    featured: true,
     title: "Digital Marketing Training in Nepal",
     image:
       "/static/Images/courses/banner/DIGITAL-MARKETING-banner-min_JBHENk4.webp",
   },
   {
+    featured: false,
     title: "Quality Assurance Training in Nepal",
     image: "/static/Images/courses/banner/qa-banner-min_l0DzbAP.jpg",
   },
   {
+    featured: true,
     title: "Graphic Design Training in Nepal",
     image: "/static/Images/courses/banner/banner11-min_6ANDad7.png",
   },
   {
+    featured: false,
     title: "Flutter Training in Nepal",
     image: "/static/Images/courses/banner/flutter-banner-min_DUDTPdH.jpg",
   },
   {
+    featured: false,
     title: "DevOps Training in Nepal",
     image: "/static/Images/courses/banner/DevOps_Training.png",
   },
 ];
 
+
 createRoot(document.getElementById("root")).render(
   <div>
+    <h1>FEATURED</h1>
+
+    <br />
     <h1>Courses</h1>
     <p>{description}</p>
     <ul>
-      <li>html</li>
-      <li>css</li>
-      <li>js</li>
-      <li>react</li>
-      <li>node</li>
+      <li>
+        <img height="100" src="https://mindrisers.com.np/static/Images/courses/banner/python-banner-min_7hCoIdR.jpg" />
+        <p>MERRRRNNNN</p>
+        <p>feature: yes</p>
+      </li>
+
+      <li>
+        <img height="100" src={domain + courses[0].image} />
+        <p>{courses[0].title}</p>
+        <p>feature: {courses[0].featured ? "yes" : "no"}</p>
+      </li>
+
+      <li>
+        <img height="100" src={domain + courses[1].image} />
+        <p>{courses[1].title}</p>
+        <p>feature: {courses[1].featured ? "yes" : "no"}</p>
+      </li>
+
+      <li>
+        <img height="100" src={`${domain}${courses[2].image}`} />
+        <p>{courses[2].title}</p>
+        <p>feature: {courses[2].featured ? "yes" : "no"}</p>
+      </li>
+
+      <li>
+        <img height="100" src={`${domain}${courses[3].image}`} />
+        <p>{courses[3].title}</p>
+        <p>feature: {courses[3].featured ? "yes" : "no"}</p>
+      </li>
     </ul>
+
+    <h2></h2>
   </div>,
 );
