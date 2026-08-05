907e388acbbbd0219d09eb7872b2b16772c3775a
dev.sagartmg <dev.sagartmg@gmail.com>
2026-08-05 08:48:19 +0545

    fix: async await for forEach, dbTransaction, esewa integration

--- chaitra-ecommerce-api/controllers/order.js
+++ chaitra-ecommerce-api/controllers/order.js
@@ -1,22 +1,27 @@
 const { Op } = require("sequelize");
+const crypto = require("crypto");
 const Order = require("../models/Order");
 const Product = require("../models/Product");
 const User = require("../models/User");
 const SubOrder = require("../models/SubOrder");
 const OrderItem = require("../models/OrderItem");
 const Cart = require("../models/Cart");
+const sequelize = require("../connections/database");
 
 const getOrders = async (req, res) => {
-  let orders = await Order.findAll({
-  }); // select * from orders
+  let orders = await Order.findAll({}); // select * from orders
   res.send({
     data: orders,
   });
 };
 
 const storeOrder = async (req, res) => {
+  const t = await sequelize.transaction();
+
   try {
     let date = new Date();
+    let totalShippingCharge = 0;
+    let totalProductPrice = 0;
     const realMonth = date.getMonth() + 1;
     let orderNo =
       "DRZ-" +
@@ -24,16 +29,17 @@ const storeOrder = async (req, res) => {
       "-" +
       Date.now() +
       Math.floor(Math.random() * 1000000);
-    let order = await Order.create({
-      orderNo,
-      paymentMode: "cod",
-      userId: req.user.id,
-      address: req.body.address,
-      phone: req.body.phone,
-    });
 
-    // order
-    console.log(order.id);
+    let order = await Order.create(
+      {
+        orderNo,
+        paymentMode: req.body.paymentMode,
+        userId: req.user.id,
+        address: req.body.address,
+        phone: req.body.phone,
+      },
+      { transaction: t },
+    );
 
     let products = req.body.products;
     // console.log(products);
@@ -45,12 +51,7 @@ const storeOrder = async (req, res) => {
       attributes: ["productId", "quantity"],
     });
 
-    // cartPoducts.find(el => el.productId == 5).quantity
-    // res.send(cartPoducts);
-    // return;
-
     let cartProductIds = cartPoducts.map((el) => el.productId);
-    console.log(cartProductIds);
 
     let sellers = await User.findAll({
       include: {
@@ -64,38 +65,77 @@ const storeOrder = async (req, res) => {
       },
     });
 
-    // for (i = 0 ; i<sellers.length)
-    // for in
-    // for of
-    sellers.forEach(async (seller) => {
-      console.log(11111);
-      let subOrder = await SubOrder.create({
-        orderId: order.id,
-        sellerId: seller.id,
-        shippingCharge: seller.shippingCharge,
-      });
+    // sellers.forEach(async (seller) => {
+    for (const seller of sellers) {
+      let subOrder = await SubOrder.create(
+        {
+          orderId: order.id,
+          sellerId: seller.id,
+          shippingCharge: seller.shippingCharge,
+        },
+        { transaction: t },
+      );
 
-      //  for (j = 0,j<seller.product.lenth ;)
-      // for in
-      // for of
-      seller.products.forEach(async (product) => {
-        await OrderItem.create({
-          subOrderId: subOrder.id,
-          productId: product.id,
-          quantity: cartPoducts.find((el) => el.productId == product.id)
-            .quantity,
-          price: product.price,
-          productName: product.title,
-        });
-      });
-    });
+      totalShippingCharge += seller.shippingCharge;
 
-    // SubOrder.create()
-    // OrderItem.create()
+      // seller.products.forEach(async (product) => {
+      for (const product of seller.products) {
+        let productQty = cartPoducts.find(
+          (el) => el.productId == product.id,
+        ).quantity;
+        await OrderItem.create(
+          // await OrderItem.bulkCreate(
+          {
+            subOrderId: subOrder.id,
+            productId: product.id,
+            // productId: null,
+            quantity: productQty,
+            price: product.price,
+            productName: product.title,
+          },
+          { transaction: t },
+        );
 
+        totalProductPrice += productQty * product.price;
+      }
+    }
+
+    let esewaPayload = undefined;
+
+    let totalAmount = totalProductPrice + totalShippingCharge;
+
+    if ((req.paymentMode = "esewa")) {
+      productCode = "EPAYTEST";
+      const message = `total_amount=${totalAmount},transaction_uuid=${orderNo},product_code=${productCode}`;
+
+      let signature = crypto
+        .createHmac("sha256", "8gBm/:&EnhH.1/q")
+        .update(message)
+        .digest("base64");
+
+      esewaPayload = {
+        amount: totalAmount,
+        tax_amount: 0,
+        total_amount: totalAmount,
+        transaction_uuid: orderNo,
+        product_code: productCode,
+        product_service_charge: 0,
+        product_delivery_charge: 0,
+        succeess_url: "http://localhost:5173/order-success",
+        failure_url: "http://localhost:5173/order-failture",
+        signed_field_names: "total_amount,transaction_uuid,product_code",
+        signature: signature,
+      };
+    }
+
+    await t.commit();
     console.log(2222222222);
-    res.send("order c");
+    res.send({
+      msg: "order created",
+      esewaPayload,
+    });
   } catch (err) {
+    await t.rollback();
     res.status(500).send({
       msg: "SERVER error",
       error: err.error,
--- chaitra-ecommerce-api/routes/order.js
+++ chaitra-ecommerce-api/routes/order.js
@@ -7,10 +7,42 @@ const {
 } = require("../controllers/order");
 
 const checkAuthentication = require("../middlewares/checkAuthentication");
+const Order = require("../models/Order");
+const { default: axios } = require("axios");
 const OrderRouter = express.Router();
 
 OrderRouter.get("/", checkAuthentication, getOrders);
 OrderRouter.post("/", checkAuthentication, storeOrder);
+
+OrderRouter.get("/verification", async (req, res) => {
+  const base64 = req.query.token;
+
+  // Decode Base64
+  const decodedString = Buffer.from(base64, "base64").toString("utf8");
+
+  // Parse JSON
+  const data = JSON.parse(decodedString);
+
+
+  let response = await axios.get(
+    `https://rc.esewa.com.np/api/epay/transaction/status/?product_code=EPAYTEST&total_amount=${data.total_amount}&transaction_uuid=${data.transaction_uuid}`,
+  );
+
+  if (response.data.status == "COMPLETE") {
+    await Order.update(
+      {
+        paymentStatus: "paid",
+      },
+      {
+        where: {
+          orderNo: data.transaction_uuid,
+        },
+      },
+    );
+  }
+
+  res.send("success");
+});
 OrderRouter.put("/:id", checkAuthentication, updateOrder);
 OrderRouter.delete("/:id", checkAuthentication, deleteOrder);
 
