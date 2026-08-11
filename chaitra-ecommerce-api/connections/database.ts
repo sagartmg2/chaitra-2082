a79ada19cfcc78f836cdfe551a3a08598d8f6cc8
dev.sagartmg <dev.sagartmg@gmail.com>
2026-08-11 08:24:51 +0545

    update pg connection for vercel

--- chaitra-ecommerce-api/connections/database.ts
+++ chaitra-ecommerce-api/connections/database.ts
@@ -1,14 +1,13 @@
-import { Sequelize} from "sequelize";
-
-// import pg from "pg"
+import { Sequelize } from "sequelize";
+import pg from "pg";
 
 const sequelize = new Sequelize(
   `postgres://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:5432/${process.env.DB_NAME}`,
   {
     logging: false,
-    // dialectModule: pg
+    dialectModule: pg,
   },
 );
 
 // module.exports = sequelize
-export default sequelize
+export default sequelize;
